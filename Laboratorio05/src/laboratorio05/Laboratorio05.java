f3c891838f8f4c41c737c5a8c351f4cdd2c194bf
MichaelQR24 <141574326+MichaelQR24@users.noreply.github.com>
2023-09-21 23:07:43 -0500

    Add files via upload

---
+++ Laboratorio05/src/laboratorio05/Laboratorio05.java
@@ -0,0 +1,13 @@
+
+package laboratorio05;
+
+public class Laboratorio05 {
+
+    public static void main(String[] args) {
+          
+        FrameListaEstudiante estudiante = new FrameListaEstudiante();
+        estudiante.setLocationRelativeTo(null);
+        estudiante.setVisible(true);
+    }
+    
+}
